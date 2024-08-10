9fd926cccac0eb4b8e79d2afab2d0ec5286fcdcc
Emmanuel Ani <emmanuelani234@gmail.com>
2024-08-10 14:58:51 +0100

    inital commit

---
+++ simple.py
@@ -0,0 +1,38 @@
+# Build a simple LLM application
+
+# importing necessary modules
+import os
+import groq
+
+from load_dotenv import load_dotenv
+load_dotenv()
+
+
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+
+# instantiating a groq class - working with groq
+
+# creating a client
+groq_client = groq.Groq(api_key = GROQ_API_KEY)
+
+sys_prompt = "You are a helpful virtual assistant, \
+    your goal is to provide useful and relevant responses to my queries"
+
+modes = [
+    "llama-3.1-405b-reasoning", 
+    "llama-3.1-70b-versatile", 
+    "llama-3.1-8b-instant", 
+    "mixtral-8x7b-32768"
+]
+
+# defining a function that takes a model as an argument
+
+def generate(model, query):
+
+    response = groq_client.chat.completion.create(
+        model=model[0],
+        messages=[
+            
+        ]
+    )
+
